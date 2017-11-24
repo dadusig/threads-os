c5526b86a33d89142d6bf9d316f59f39b002ac65
Damianos Dumi Sigalas <nsigalas@ceid.upatras.gr>
2017-11-24 18:08:42 +0200

    Add multithread_1.c ALL OK!

--- multithread_1.c
+++ multithread_1.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include "util.h"
 
-int main()
+typedef struct
 {
-  display("This is a dummy implementation.");
-  return 0;
+    int rep;
+    char* string;
+}thread_args_t;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* thread_runner(void *p)
+{
+
+    thread_args_t *param = (thread_args_t *) p;
+
+    for (int i = 0; i < param->rep; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        display(param->string);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    thread_args_t params[argc-2];
+
+    pthread_t tid[argc-2];
+
+    for (int i = 2; i < argc; i++)
+    {
+        params[i-2].string = argv[i];
+        params[i-2].rep = atoi(argv[1]);
+        pthread_create(&tid[i-2], NULL, thread_runner, &params[i-2]);
+    }
+
+    for (int i = 0; i < argc-2; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
 }
