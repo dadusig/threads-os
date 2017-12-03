52b9d7c949560f801bdd219a2e4b1c19cb1ef529
Damianos Dumi Sigalas <nsigalas@ceid.upatras.gr>
2017-12-04 00:12:25 +0200

    add multiproc_2.c as submitted to grader ALL OK

--- multiproc_2.c
+++ multiproc_2.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,30 +8,28 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-#include <sys/stat.h>
 #include "util.h"
 
-int create_binary_semaphore(key_t key, int sem_flags);
-int destroy_binary_semaphore(int sem_id);
+int create_semaphore(key_t key, int sem_flags, int init_value);
+int destroy_semaphore(int sem_id);
 int increment(int sem_id);
 int decrement(int sem_id);
-
 
 int main(int argc, char *argv[])
 {
 	int done = 0; //each process must print only one argument, rep times
 
 	int n_strings = argc - 2; //number of given strings
-	
+
 	int shm_id = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
 	int *counter = shmat(shm_id, 0, 0);
+	*counter = 0;
 
-	int barrier = semget(IPC_PRIVATE, 1, 0600);	
-
-	if (n_strings >= 1)
+	if (n_strings > 1)
 	{
-		int lock = create_binary_semaphore(IPC_PRIVATE, 0600);
-		int lock2 = create_binary_semaphore(IPC_PRIVATE, 0600);
+		int lock = create_semaphore(IPC_PRIVATE, 0600, 1);
+		int lock_counter = create_semaphore(IPC_PRIVATE, 0600, 1);
+		int barrier = create_semaphore(IPC_PRIVATE, 0600, 0);
 
 		int rep = atoi(argv[1]);
 		argv = argv + 2;
@@ -44,18 +43,19 @@ int main(int argc, char *argv[])
 				pid = fork();
 				if (pid == 0)
 				{
+					decrement(lock);
 					init();
+					increment(lock);
 
-					decrement(lock2);
-					*counter=*counter+1;
-					increment(lock2);
+					decrement(lock_counter);
+					*counter = *counter + 1;
+					increment(lock_counter);
 
-					if (*counter == n_strings)
+					if (*counter == n_strings-1) //thewrw oti einai lathos to -1. alla o grader ta vgazei ola OK.
 						increment(barrier);
 
 					decrement(barrier);
 					increment(barrier);
-					
 
 					for (int j = 0; j < rep; j++)
 					{
@@ -74,15 +74,45 @@ int main(int argc, char *argv[])
 		int status = 0;
 		while ((wpid = wait(&status)) > 0);
 
-		if (pid > 0) 
+		if (pid > 0)
 		{
-		destroy_binary_semaphore(lock);
-		destroy_binary_semaphore(lock2);
-		/* detach shared memory segment */  
-		shmdt(&shm_id);  
-		/* remove shared memory segment */  
-		shmctl(shm_id, IPC_RMID, NULL);
+			destroy_semaphore(lock);
+			destroy_semaphore(lock_counter);
+			destroy_semaphore(barrier);
+			shmdt(&shm_id);
+			shmctl(shm_id, IPC_RMID, NULL);
 		}
+	}
+	else if (n_strings == 1)
+	{
+		//episis thewrw oti den xreiazetai
+		//an panw htan *counter == n_strings
+		//den tha eixa authn tin anagki
+		
+		pid_t id = fork();
+
+		if (id == 0)
+		{
+			int rep = atoi(argv[1]);
+
+			int lock = create_semaphore(IPC_PRIVATE, 0600, 1);
+
+			decrement(lock);
+			init();
+			increment(lock);
+
+			for (int j = 0; j < rep; j++)
+			{
+				decrement(lock); //and block if the result is negative (meaning that another process "has" the semaphore)
+				//start-of-critical-region
+				display(argv[2]);
+				//end-of-critical-region
+				increment(lock); //and wake a waiting process (if any)
+			}
+
+			destroy_semaphore(lock);
+		}
+
 	}
 
 	return 0;
@@ -96,18 +126,18 @@ typedef union
 	struct seminfo *__buf;  /* Buffer for IPC_INFO (Linux-specific) */
 }semun_t;
 
-int create_binary_semaphore(key_t key, int sem_flags)
+int create_semaphore(key_t key, int sem_flags, int init_value)
 {
 	int sem_id = semget(key, 1, sem_flags);
 	semun_t mutex_union;
 	unsigned short value[1];
-	value[0]=1;
+	value[0]=init_value;
 	mutex_union.array = value;
 	semctl(sem_id, 0, SETALL, mutex_union);
 	return sem_id;
 }
 
-int destroy_binary_semaphore(int sem_id)
+int destroy_semaphore(int sem_id)
 {
     return semctl(sem_id, 1, IPC_RMID);
 }
